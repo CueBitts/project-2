84f95c1c8389be57730488b800449d3b74c86444
qhogan <gitASkl0921[1;6Dquinlanhogan@gmail.com>
2022-04-29 21:51:21 -0700

    heroku13

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use('/restaurants', restaurants)
 //       });
 //     }
 
-if(process.nextTick.NODE_ENV === 'production') {
+if(process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'))
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))

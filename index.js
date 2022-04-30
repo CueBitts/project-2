f4bf3587684feb2ac7209f76c0e56d00a63de221
qhogan <gitASkl0921[1;6Dquinlanhogan@gmail.com>
2022-04-29 21:39:29 -0700

    heroku10

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ app.use(express.urlencoded({extended: true}))
 app.use(cors())
 app.use('/restaurants', restaurants)
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("build"));
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+    });
+  }
+
 // app.listen(3000, () => {
 //     console.log('listening on port 3000')
 // })

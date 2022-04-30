c4eb66d941927396010e678f3cb53baf51759247
qhogan <gitASkl0921[1;6Dquinlanhogan@gmail.com>
2022-04-29 20:12:31 -0700

    heroku3

--- backend/index.js
+++ backend/index.js
@@ -12,8 +12,12 @@ app.use('/restaurants', restaurants)
 //     console.log('listening on port 3000')
 // })
 
-app.set('port', process.env.PORT || 3000)
+// app.set('port', process.env.PORT || 3000)
 
-app.listen(app.get('port'), () => {
-    console.log(`Listening on port ${app.get('port')}`)
+// app.listen(app.get('port'), () => {
+//     console.log(`Listening on port ${app.get('port')}`)
+// })
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log(`Listening on port ${this.address().port}`)
 })
